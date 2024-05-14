2f06348df3183d9d995b123a4b64010b2584ba43
Himanshu <140253431+Himanshu-18-P@users.noreply.github.com>
2024-05-14 17:11:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,29 +2,23 @@ from flask import Flask , request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app , resources={r"/*" : {
-    "origins" : "*", 
-    "methods" : ["GET" , "POST"]
+CORS(app , resources={r"/api/*" : {
+    "origins" : "*"
     }})
 
-api_v2_cors_config = {
-  "origins": ["http://localhost:5000"],
-  "methods": ["OPTIONS", "GET", "POST"],
-  "allow_headers": ["Authorization", "Content-Type"]
-}
 
 @app.route('/')
 def index():
     print('hello world')
     
 
-@app.route('/api/v2/hello')
-@cross_origin(**api_v2_cors_config)
+@app.route('/api/hello' , methods=['POST'])
 def v2_hello():
-    param1 = request.args.get('name')
-    param2 = request.args.get('phone')
+    data = request.json 
+    param1 = data['name']
+    param2 = data['phone']
     print(param1 , param2)
-    return 'or bhai'
+    return {'a' : [param1 , param2]}
 
 
 if __name__ == '__main__':
